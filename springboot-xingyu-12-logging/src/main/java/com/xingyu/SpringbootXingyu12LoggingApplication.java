25cdb8a604959bc5e6c31e3224db760ec6797ac8
SuYuJavaAndPython <3181647371@qq.com>
2021-05-23 14:23:20 +0800

    springboot-12-logging_update

--- springboot-xingyu-12-logging/src/main/java/com/xingyu/SpringbootXingyu12LoggingApplication.java
+++ springboot-xingyu-12-logging/src/main/java/com/xingyu/SpringbootXingyu12LoggingApplication.java
@@ -13,6 +13,27 @@ public class SpringbootXingyu12LoggingApplication {
      *         %logger{50} 表示logger名字最长50个字符，否则按照句点分割。 
      *         %msg：日志消息，
      *         %n是换行符
+     * 现在流行的日志框架搭配是 slf4j日志框架门面 + logback
+     * 想要使用log4j12：先移除logback和log4j，再加入log4j12
+     *          <dependency>
+     *             <groupId>org.springframework.boot</groupId>
+     *             <artifactId>spring-boot-starter-web</artifactId>
+     *             <exclusions>
+     *                 <exclusion>
+     *                     <artifactId>logback-classic</artifactId>
+     *                     <groupId>ch.qos.logback</groupId>
+     *                 </exclusion>
+     *                 <exclusion>
+     *                     <artifactId>log4j-to-slf4j</artifactId>
+     *                     <groupId>org.apache.logging.log4j</groupId>
+     *                 </exclusion>
+     *             </exclusions>
+     *          </dependency>
+     *
+     *         <dependency>
+     *             <groupId>org.slf4j</groupId>
+     *             <artifactId>slf4j-log4j12</artifactId>
+     *         </dependency>
      * @param args
      */
     public static void main(String[] args) {
